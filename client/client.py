1c2100246c0ce9f7cb968b45da9ae4000b252996
essipassoja <epassoja18@student.oulu.fi>
2023-03-18 17:42:10 +0200

    Fixed messaging issues between server and clients

--- client/client.py
+++ client/client.py
@@ -3,110 +3,76 @@ Copyright: Essi Passoja
 """
 
 import socket
-import mysql.connector
-from mysql.connector import Error
+import threading
 from tkinter import *
+import mysql.connector
+
+HOST = "server"
+PORT = 5000
 
 class Client:
     def __init__(self):
-        self.db = None
+        # Initialize attributes
         self.cursor = None
+        self.db = None
         self.photo = None
         self.player = None
+        self.socket = None
+
+        # Initialize GUI
         self.root = Tk()
         self.root.title("Tic-tac-toe")
-        self.socket = socket.socket()
-        self.transaction_in_progress = False
 
-    ####################  MySQL  #######################
+        # Start listening to the server and ask for player info
+        self.get_player_role()
+        self.listen_server = threading.Thread(
+            target=self.server_listener)
+        self.listen_server.start()
 
-    def check_that_game_table_exists(self):
-        self.connect_to_database()
-        result = self.execute_query("SHOW TABLES LIKE 'game'")
-        self.transaction_in_progress = True
-        if not result:
-            self.create_table()
-    
-    def execute_query(self, query):
-        self.cursor.execute(query)
-        result = self.cursor.fetchall()
-        print("result: {}".format(result))  # debug
-        return result
-    
-    def connect_to_database(self):
-        self.db = mysql.connector.connect(
-            host = "mysql",
-            port="3306",
-            user="root",
-            password="root",
-            database="tictactoe_db"
-        )
-        self.cursor = self.db.cursor()
-
-    def create_table(self):
-        try:
-            with open('game.sql', 'r') as file:
-                script = file.read()
-                print("Found table script: {}".format(script))
-            self.execute_query(script)
-            print("Table created successfully.")
-        except Error as e:
-            print(f"The error '{e}' occurred while creating the table.")
-
-    #####################  Game GUI  #######################
+    # ~~~~~~~~~~~~~~  Game GUI  ~~~~~~~~~~~~~~~~
 
     def create_gui(self):
         canvas = Canvas(self.root)
         canvas.pack(fill=BOTH, expand=1)
         background = PhotoImage(file="tictactoe_background.png")
         self.photo = (PhotoImage(file="tic.png") if self.player == "tic" 
-                          else PhotoImage(file="tac.png"))
+                      else PhotoImage(file="tac.png"))
         canvas.create_image(0, 0, anchor=NW, image=background)
         self.root.wm_geometry("656x656")
         canvas.bind("<Button-1>", self.handle_click)
         self.root.mainloop()
     
     def handle_click(self, event):
-        canvas = event.widget
+        # canvas = event.widget
         x, y = self.check_click_and_return_image_coordinates(event.x, event.y)
         if x != 0 and y != 0:
-            if not self.transaction_in_progress:
-                self.start_transaction()
-            self.send_prepare_command_to_server(x, y)
-            response = self.socket.recv(1024).decode()
-            if response == "yes":
-                self.send_commit_command_to_server()
-                canvas.create_image(x, y, anchor=NW, image=self.photo)
-                self.commit_transaction()
-                self.transaction_in_progress = False
-            elif response == "no":
-                self.send_abort_command_to_server()
-                self.rollback_transaction()
-                self.transaction_in_progress = False
+            location = self.coordinate_to_square_location((x, y))
+            self.send_prepare_command_to_server(location)
+            # TODO: canvas.create_image(x, y, anchor=NW, image=self.photo)
 
     @staticmethod
     def check_click_and_return_image_coordinates(x, y):
-        if 63 < x < 226 and 135 < y < 275:
+        if 63 < x < 226 and 135 < y < 275:  # Square (1, 1)
             return 65, 135
-        elif 249 < x < 426 and 140 < y < 270:
+        elif 249 < x < 426 and 140 < y < 270:  # Square (1, 2)
             return 270, 136
-        elif 450 < x < 630 and 141 < y < 280:
+        elif 450 < x < 630 and 141 < y < 280:  # Square (1, 3)
             return 470, 134
-        elif 58 < x < 218 and 305 < y < 435:
+        elif 58 < x < 218 and 305 < y < 435:  # Square (2, 1)
             return 65, 310
-        elif 240 < x < 444 and 296 < y < 440:
+        elif 240 < x < 444 and 296 < y < 440:  # Square (2, 2)
             return 270, 309
-        elif 462 < x < 618 and 300 < y < 445:
+        elif 462 < x < 618 and 300 < y < 445:  # Square (2, 3)
             return 470, 311
-        elif 61 < x < 215 and 463 < y < 595:
+        elif 61 < x < 215 and 463 < y < 595:  # Square (3, 1)
             return 65, 475
-        elif 241 < x < 442 and 471 < y < 593:
+        elif 241 < x < 442 and 471 < y < 593:  # Square (3, 2)
             return 270, 475
-        elif 466 < x < 617 and 467 < y < 587:
+        elif 466 < x < 617 and 467 < y < 587:  # Square (3, 3)
             return 470, 475
         else:
-            return 0, 0
-    
+            return 0, 0  # Outside the square grid
+
     @staticmethod
     def coordinate_to_square_location(coordinates):
         if coordinates == (65, 135):
@@ -149,44 +115,76 @@ class Client:
         elif location == (3, 3):
             return 470, 475
     
-    #############  Server communication  #################
-    
-    def connect_to_server(self):
-        port = 5000
-        self.socket.connect(("server", port))
-    
-    def get_player_information(self):
-        self.send_command_to_server("Who am I?")
-    
-    def send_command_to_server(self, command):
-        self.socket.sendall(command.encode())
-        response = self.socket.recv(1024).decode()
-        print("Response from server: {}".format(response))
+    # ~~~~~~~~~~~  Server communication  ~~~~~~~~~~~~~
 
-    def send_prepare_command_to_server(self, x, y):
-        self.send_command_to_server("prepare -> {}, {}".format(x, y))
+    @staticmethod
+    def connect_to_server():
+        s = socket.socket()
+        s.connect((HOST, PORT))
+        return s
     
-    def start_transaction(self):
-        self.db.start_transaction()
+    def send_message_to_server(self, command):
+        print("Sending command '{}' to server.".format(command))
+        s = self.connect_to_server()
+        s.sendall(command.encode())
+        response = s.recv(1024).decode()
+        s.close()
+        if response:
+            print("Received a response from server: {}".format(response))
+            return response
+        return ""
+
+    def get_player_role(self):
+        player_role = self.send_message_to_server("Who am I?")
+        self.player = player_role
+
+    def send_prepare_command_to_server(self, location):
+        self.send_message_to_server(
+            "prepare {} -> {}".format(self.player, location))
     
     def send_commit_command_to_server(self):
-        self.send_command_to_server("commit")
-    
-    def commit_transaction(self):
-        self.db.commit()
+        self.send_message_to_server("commit")
 
     def send_abort_command_to_server(self):
-        self.send_command_to_server("abort")
+        self.send_message_to_server("abort")
     
-    def rollback_transaction(self):
-        self.db.rollback()
+    def server_listener(self):
+        self.socket = self.connect_to_server()
+        while True:
+            message = self.socket.recv(1024).decode()
+            if message:
+                print("Received a message from server: {}".format(message))
+                self.handle_message(message)
+    
+    def handle_message(self, message):
+        if message == "Request to prepare":
+            # TODO: When is this no?
+            self.send_message_to_server("yes")
+        elif message == "prepared":
+            self.send_commit_command_to_server()
 
-    ####################  Main  #####################
+    # ~~~~~~~~~~~~~~  MySQL  ~~~~~~~~~~~~~~~~
+    
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print("result: {}".format(result))  # debug
+        return result
+    
+    def connect_to_database(self):
+        self.db = mysql.connector.connect(
+            host = "mysql",
+            port="3306",
+            user="root",
+            password="root",
+            database="tictactoe_db"
+        )
+        self.cursor = self.db.cursor()
+
+    # ~~~~~~~~~~~~~~  Main  ~~~~~~~~~~~~~~~~
     
     def main(self):
-        self.connect_to_server()
-        self.get_player_information()
-        self.check_that_game_table_exists()
+        self.connect_to_database()
         self.create_gui()
 
 if __name__ == '__main__':
--- server/server.py
+++ server/server.py
@@ -3,52 +3,95 @@ Copyright: Essi Passoja
 """
 
 import socket
+import threading
+import mysql.connector
+from mysql.connector import Error
+
+PORT = 5000
+HOST = ""
+HOST_CLIENT1 = "client1"  # Docker container for client1
+HOST_CLIENT2 = "client2"  # Docker container for client2
+
+# https://stackoverflow.com/questions/57925492/how-to-listen-continuously-to-a-socket-for-data-in-python
 
 class Server:
     def __init__(self):
-        self.game_board = [[' ', ' ', ' '], [' ', ' ', ' '], [' ', ' ', ' ']]
-        self.game_started = False
+        # Initialize db attributes
+        self.db = None
+        self.cursor = None
+        self.transaction_in_progress = False
+
+        #Initialize game play attributes
+        self.current_player = "tic"
+        self.requested_move = ""
+        self.game_board = [
+            [' ', ' ', ' '], 
+            [' ', ' ', ' '], 
+            [' ', ' ', ' ']]
         self.players = []
-        self.socket = socket.socket()
 
-    def start_listening_socket(self):
-        port = 5000
-        self.socket.bind(("", port))
-        self.socket.listen(5)
+    # ~~~~~~~~  Client communication  ~~~~~~~~~~~~
 
+    def start_listening_to_port(self):
+        s = socket.socket()
+        s.bind((HOST, PORT))
+        s.listen(5)
         while True:
-            connection, address = self.socket.accept()
+            connection, address = s.accept()
             print("Got connection from {}".format(address))
-            message = bytes.decode(connection.recv(1024))
-            print(message)
-            if not self.game_started:
-                self.check_for_players(connection, message)
-            ### Phase 1 ###
-            if "prepare" in message:
-                self.prepare_phase(connection)
-            ### Phase 2 ###
-            elif "commit" in message:
-                self.commit_phase(connection)
-                # TODO check if current player is winner
-            elif "abort" in message:
-                self.abort_phase(connection)
+            t = threading.Thread(
+                target=self.handle_client_messages, 
+                args=(connection,))
+            t.start()
 
-    def check_for_players(self, connection, message):
-        if "Who am I?" in message:
-            if not self.players:
-                message = "tic"
-                connection.sendall(message.encode())
-                self.players.append("tic")
-            else:
-                message = "tac"
-                connection.sendall(message.encode())
-                self.players.append("tac")
-                self.game_started = True
-    
+    def handle_client_messages(self, connection):
+        try:
+            while True:
+                message = bytes.decode(connection.recv(1024))
+                ### Phase 1 ###
+                if ("prepare" in message and 
+                        self.current_player in message and 
+                        len(self.players) == 2):
+                    self.requested_move = message
+                    self.prepare_phase(connection)
+                elif "prepare" in message:
+                    # Game has not started or it's not the player's turn
+                    connection.send("Refusing to make the move".encode())
+                ### Phase 2 ###
+                elif "commit" in message and len(self.players) == 2:
+                    self.commit_phase(connection)
+                    self.read_move_location(message)
+                elif "abort" in message and len(self.players) == 2:
+                    self.abort_phase(connection)
+                elif "Who am I" in message:
+                    self.send_player_info(connection)
+        except BrokenPipeError:
+            print("Connection was probably closed by client.")
+        finally:
+            print("Closing the connection.")
+            connection.close()
+
+    # ~~~~~~~~  Decice player marks  ~~~~~~~~~~~~
+
+    def send_player_info(self, connection):
+        if not self.players:
+            message = "tic"
+            connection.send(message.encode())
+            self.players.append("tic")
+        else:
+            message = "tac"
+            connection.send(message.encode())
+            self.players.append("tac")
+            self.game_started = True
+
+    # ~~~~~~~~  Two-phase protocol  ~~~~~~~~~~~~
+
     def prepare_phase(self, connection):
+        if not self.transaction_in_progress:
+            self.start_transaction()
         message = "Request to prepare"
         connection.sendall(message.encode())
-        reply = bytes.decode(connection.recv(1024))  # Optios: Prepared/Not Prepared
+        reply = bytes.decode(connection.recv(1024))
         if reply == "yes":
             message = "prepared"
             connection.sendall(message.encode())
@@ -56,9 +99,9 @@ class Server:
             message = "not prepared"
             connection.sendall(message.encode())
 
-
+    @staticmethod
     def commit_phase(self, connection):
-        message = "commit?"
+        message = "Request to commit"
         connection.sendall(message.encode())
         reply = bytes.decode(connection.recv(1024))
         if reply == "yes":
@@ -69,15 +112,72 @@ class Server:
             message = "commit aborted"
             connection.sendall(message.encode())
 
-    def abort_phase(self, connection):
+    @staticmethod
+    def abort_phase(connection):
         message = "abort confirmed"
         connection.sendall(message.encode())
 
     def update_database(self):
         pass
 
+    # ~~~~~~~~~~~~~~  MySQL  ~~~~~~~~~~~~~~~~
+
+    def connect_to_database_and_check_tables(self):
+        print("Checking the DB")  # debug
+        self.connect_to_database()
+        result = self.execute_query("SHOW TABLES LIKE 'game'")
+        self.transaction_in_progress = True
+        if not result:
+            self.create_table()
+    
+    def execute_query(self, query):
+        self.cursor.execute(query)
+        result = self.cursor.fetchall()
+        print("result: {}".format(result))  # debug
+        return result
+    
+    def connect_to_database(self):
+        self.db = mysql.connector.connect(
+            host = "mysql",
+            port="3306",
+            user="root",
+            password="root",
+            database="tictactoe_db"
+        )
+        self.cursor = self.db.cursor()
+
+    def create_table(self):
+        try:
+            with open('game.sql', 'r') as file:
+                script = file.read()
+                print("Found table script: {}".format(script))
+            self.execute_query(script)
+            print("Table created successfully.")
+        except Error as e:
+            print(f"The error '{e}' occurred while creating the table.")
+
+    def start_transaction(self):
+        self.db.start_transaction()
+
+    def commit_transaction(self):
+        self.db.commit()
+    
+    def rollback_transaction(self):
+        self.db.rollback()
+
+    # ~~~~~~~~~~~~  Game logic  ~~~~~~~~~~~~~~
+
+    def read_move_location(message):
+        pass
+
+    def check_game_status():
+        pass
+
+    # ~~~~~~~~~~~~~~  Main  ~~~~~~~~~~~~~~~~
+
     def main(self):
-        self.start_listening_socket()
+        self.connect_to_database_and_check_tables()
+        self.start_listening_to_port()
 
 if __name__ == '__main__':
     server = Server()
