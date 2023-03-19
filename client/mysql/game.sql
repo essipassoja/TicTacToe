d1d307b7e60aa9e4432f668a112f7954c7197702
essipassoja <epassoja18@student.oulu.fi>
2023-03-19 15:02:03 +0200

    Updated game.sql

--- client/mysql/game.sql
+++ client/mysql/game.sql
@@ -1,6 +1,5 @@
 CREATE TABLE IF NOT EXISTS game (
   id INT AUTO_INCREMENT PRIMARY KEY,
   game_table TEXT,
-  game_status ENUM('Not started', 'On-going', 'Finished') DEFAULT 'Not started',
   winner ENUM('None', 'tic', 'tac') DEFAULT 'None'
 );
